fd62b10901001a1fdafc44618e4054750c588973
faramin esmaili seraji <faramin.smile@gmail.com>
2017-07-14 17:14:17 +0430

    Make telegram logger optinal.

--- common/logger.go
+++ common/logger.go
@@ -6,33 +6,30 @@ import (
 )
 
 func init() {
-	var (
-		chatID = ConfigInt64("TELEGRAM_CHATID")
-		token  = ConfigString("TELEGRAM_TOKEN")
-	)
-
-	if token == "" {
-		log.Fatalln("TELEGRAM_TOKEN not presented.")
-	}
-
-	if chatID == int64(0) {
-		log.Fatalln("TELEGRAM_CHATID not presented.")
-	}
-
 	if level, err := log.ParseLevel(ConfigString("LOG_LEVEL")); err == nil {
 		log.SetLevel(level)
 	} else {
 		log.SetLevel(log.DebugLevel)
 	}
 
-	telegramHooker := telegrus.NewHooker(token, chatID)
+	var (
+		chatID = ConfigInt64("TELEGRAM_CHATID")
+		token  = ConfigString("TELEGRAM_TOKEN")
+	)
+
+	if token == "" {
+		log.Warnln("TELEGRAM_TOKEN not presented. Telegram logger DISABLED.")
+		return
+	}
+
+	if chatID == int64(0) {
+		log.Fatalln("TELEGRAM_CHATID not presented.")
+	}
+
+	telegramHooker := telegrus.NewHooker(token, chatID).SetLevel(log.GetLevel())
 
 	if username := ConfigString("TELEGRAM_MENTION"); username != "" {
-		telegramHooker.SetMention(map[log.Level][]string{
-			log.WarnLevel:  []string{"Huberrr"},
-			log.ErrorLevel: []string{"Huberrr"},
-			log.PanicLevel: []string{"Huberrr"},
-		})
+		telegramHooker.MentionOn(log.WarnLevel, username)
 	}
 
 	log.AddHook(telegramHooker)
